80272af930d18c2854f5e8025a4527b5b98b7b29
Dan Schellenberg <schellenberg@gmail.com>
2017-10-18 09:45:22 -0600

    area of circle

---
+++ period1/area_of_circle.py
@@ -0,0 +1,12 @@
+#---------------------------------------------------
+# Area of Circle Calculator
+# Dan Schellenberg
+# Oct 18, 2017
+#---------------------------------------------------
+
+radius = input("Please enter the radius: ")
+radius = float(radius)  #turn user input into a number
+
+answer = 3.14 * radius ** 2
+
+print("The area of the circle is", answer)
