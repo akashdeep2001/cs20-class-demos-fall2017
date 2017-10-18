854fef8004fbdbe44cd7bf370ac0183bafb94b3e
Dan Schellenberg <schellenberg@gmail.com>
2017-10-18 08:34:38 -0600

    first demos

---
+++ firstDayDemo-period5.py
@@ -0,0 +1,3 @@
+some_number = 5
+if some_number > 3:
+    print("Yeah, the number is bigger.")
---
+++ secondDayDemo-period1.py
@@ -0,0 +1,6 @@
+some_name = input("Who are you?")
+
+if some_name == "Dan":
+    print("Hello, master!")
+else:
+    print("Ummm... who are you, again?")
