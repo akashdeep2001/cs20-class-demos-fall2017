6e004e9e41a3103a084e4c97ffabbe7f4b277a94
Dan Schellenberg <schellenberg@gmail.com>
2017-10-23 09:06:50 -0600

    finished practice problem

--- period1/add_or_subtract.py
+++ period1/add_or_subtract.py
@@ -4,12 +4,28 @@
 
 operation_chosen = input("Would you like to add or subtract? ")
 
-first_number = input("Please enter the first number: ")
-second_number = input("Please enter the second number: ")
-
-#convert input into numbers
-first_number = float(first_number)
-second_number = float(second_number)
-
 if operation_chosen == "add":
+    first_number = input("Please enter the first number: ")
+    second_number = input("Please enter the second number: ")
+
+    #convert input into numbers
+    first_number = float(first_number)
+    second_number = float(second_number)
+    
+    the_answer = first_number + second_number
+    print("The answer when you add is", the_answer)
+
+elif operation_chosen == "subtract":
+    first_number = input("Please enter the first number: ")
+    second_number = input("Please enter the second number: ")
+
+    #convert input into numbers
+    first_number = float(first_number)
+    second_number = float(second_number)
+    
+    the_answer = first_number - second_number
+    print("The answer when you subtract is", the_answer)
+
+else:
+    print("Error. I don't know what that means. Please enter 'add' or 'subtract'.")
     
