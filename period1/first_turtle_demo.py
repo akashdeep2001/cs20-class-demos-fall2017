af2b753b6987fdbd2b1b18fc1c68e48efd9c0b3b
Dan Schellenberg <schellenberg@gmail.com>
2017-10-30 09:58:03 -0600

    first day of turtles

---
+++ period1/first_turtle_demo.py
@@ -0,0 +1,18 @@
+import turtle
+
+win = turtle.Screen()
+win.bgcolor("lightgreen")
+
+david = turtle.Turtle()
+david.color("red")
+david.pensize(5)
+david.shape("turtle")
+
+david.forward(150)
+david.left(90)
+david.forward(75)
+
+david.left(90)
+david.forward(150)
+david.left(90)
+david.forward(75)
