050b7651de823edb8b41c293d4128c45a1747af5
Dan Schellenberg <schellenberg@gmail.com>
2017-10-30 15:11:18 -0600

    first turtle demos

---
+++ period5/first_turtle_demo.py
@@ -0,0 +1,16 @@
+import turtle
+
+the_window = turtle.Screen()
+
+luke = turtle.Turtle()
+luke.shape("turtle")
+
+luke.forward(150)
+luke.left(90)
+luke.forward(75)
+luke.left(90)
+
+luke.forward(150)
+luke.left(90)
+luke.forward(75)
+luke.left(90)
