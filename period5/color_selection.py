b791faf591373af8119e52c26d5dda73b677eb44
Dan Schellenberg <schellenberg@gmail.com>
2017-10-31 14:54:15 -0600

    turtle drawing demos

---
+++ period5/color_selection.py
@@ -0,0 +1,19 @@
+# Color Selection
+
+import turtle
+
+# create window, and set it's color
+canvas = turtle.Screen()
+the_background_color = canvas.textinput("Color", "Please enter a background color: ")
+canvas.bgcolor(the_background_color)
+
+#create the turtle, and it's attributes
+bree = turtle.Turtle()
+brees_color = canvas.textinput("Color", "Please enter the color of the turtle: ")
+bree.color(brees_color)
+bree.pensize(3)
+
+#draw!
+bree.forward(100)
+bree.right(60)
+bree.forward(100)
