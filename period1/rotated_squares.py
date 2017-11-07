b2773ba0742fc98d9aaf7999c3110d1519bac08a
Dan Schellenberg <schellenberg@gmail.com>
2017-11-07 09:25:44 -0600

    inner square questions

---
+++ period1/rotated_squares.py
@@ -0,0 +1,34 @@
+import turtle
+
+def draw_square_from_centre(some_turtle, side_length):
+    
+    #go to starting location
+    some_turtle.up()
+    for i in range(2):
+        some_turtle.right(90)
+        some_turtle.forward(side_length/2)
+    some_turtle.right(180)
+    some_turtle.down()
+    
+    #draw the square
+    for side in range(4):
+        some_turtle.forward(side_length)
+        some_turtle.left(90)
+
+    #go back to original location
+    some_turtle.up()
+    for i in range(2):
+        some_turtle.forward(side_length/2)
+        some_turtle.left(90)
+    some_turtle.left(180)
+
+window = turtle.Screen()
+window.bgcolor("lightgreen")
+
+fiza = turtle.Turtle()
+fiza.color("red")
+fiza.pensize(4)
+
+draw_square_from_centre(fiza, 200)
+fiza.left(45)
+draw_square_from_centre(fiza, 200)
