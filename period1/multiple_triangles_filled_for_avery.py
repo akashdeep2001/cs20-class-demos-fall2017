d48a84087698881214e41cc40dfb87ac15b2ae58
Dan Schellenberg <schellenberg@gmail.com>
2017-11-08 09:30:16 -0600

    extra demo with filling code

---
+++ period1/multiple_triangles_filled_for_avery.py
@@ -0,0 +1,29 @@
+import turtle
+
+def draw_triangle(some_turtle, length):
+    """Draws a triangle with some_turtle, with each side being length long."""
+    for side in range(3):
+        some_turtle.forward(length)
+        some_turtle.left(120)
+        
+def draw_hexagon_with_triangles(a_turtle, side_length):
+    '''Draw a hexagon out of triangles with a_turtle, and sides being side_length long.'''
+    for triangle in range(6):
+        if triangle % 2 == 0:
+            a_turtle.color("yellow")
+        else:
+            a_turtle.color("black")
+        a_turtle.begin_fill()
+        draw_triangle(a_turtle, side_length)
+        a_turtle.end_fill()
+        a_turtle.left(60)
+
+
+screen = turtle.Screen()
+screen.bgcolor("gray")
+
+fiza = turtle.Turtle()
+fiza.color("yellow")
+fiza.pensize(4)
+
+draw_hexagon_with_triangles(fiza, 150)
