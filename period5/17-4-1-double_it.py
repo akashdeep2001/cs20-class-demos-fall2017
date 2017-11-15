d3ad5f56b22af41e2be50adb893ef910e13ce999
Dan Schellenberg <schellenberg@gmail.com>
2017-11-15 15:09:53 -0600

    practice problem solutions

---
+++ period5/17-4-1-double_it.py
@@ -0,0 +1,2 @@
+def double_it(the_number):
+    return the_number * 2
---
+++ period5/17-4-2-double_it_positive.py
@@ -0,0 +1,5 @@
+def double_it_positive(the_number):
+    if the_number >= 0:
+        return the_number * 2
+    else:
+        return -1
