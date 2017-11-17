e4423da5e29c8f474d00956dc155dfa8705cd26e
Dan Schellenberg <schellenberg@gmail.com>
2017-11-17 15:06:38 -0600

    added turtle

--- period5/accerometer.py
+++ period5/accerometer.py
@@ -1,4 +1,5 @@
 import microbit
+import turtle
 
 def horizontal_tilt(sensitivity_amount):
     """Returns left or right, depending on which way the micro:bit is tilted. Small sensitivity_amount is more sensitive, large sensitivity_amount is less sensitive."""
@@ -11,16 +12,25 @@ def horizontal_tilt(sensitivity_amount):
         return "left"
 
 
+canvas = turtle.Screen()
+canvas.bgcolor("black")
+
+travis = turtle.Turtle()
+travis.color("white")
+travis.pensize(5)
+
 while True:
     horizontal_tilt_direction = horizontal_tilt(100)
 
     if horizontal_tilt_direction == "right":
         microbit.display.show("R")
         print("Tilted right.")
+        travis.forward(10)
 
     elif horizontal_tilt_direction == "left":
         microbit.display.show("L")
         print("Tilted left.")
+        travis.left(5)
 
     else:
         microbit.display.show("-")
