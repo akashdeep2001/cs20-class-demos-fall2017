bf26070edf56e22bc829eb6ed971ea8e924e82d6
Dan Schellenberg <schellenberg@gmail.com>
2017-11-20 15:08:59 -0600

    falling object for camilla

---
+++ period5/falling_demo.py
@@ -0,0 +1,49 @@
+import microbit
+import turtle
+
+def horizontal_tilt(sensitivity_amount):
+    """Returns left or right, depending on which way the micro:bit is tilted. Small sensitivity_amount is more sensitive, large sensitivity_amount is less sensitive."""
+    x_tilt = microbit.accelerometer.get_x()
+
+    if x_tilt > sensitivity_amount:
+        return "right"
+
+    elif x_tilt < -1 * sensitivity_amount:
+        return "left"
+
+
+canvas = turtle.Screen()
+canvas.bgcolor("black")
+canvas.register_shape("ship.gif")
+
+travis = turtle.Turtle()
+travis.color("white")
+travis.penup()
+travis.shape("ship.gif")
+travis.pensize(5)
+
+ball = turtle.Turtle()
+ball.color("red")
+ball.penup()
+ball.shape("circle")
+ball.goto(100, 300)
+ball.right(90)
+
+while True:
+    ball.forward(2)
+    
+    horizontal_tilt_direction = horizontal_tilt(100)
+
+    if horizontal_tilt_direction == "right":
+        microbit.display.show("R")
+        print("Tilted right.")
+        travis.forward(10)
+
+    elif horizontal_tilt_direction == "left":
+        microbit.display.show("L")
+        print("Tilted left.")
+        travis.backward(10)
+
+    else:
+        microbit.display.show("-")
+        print("Flat!")
