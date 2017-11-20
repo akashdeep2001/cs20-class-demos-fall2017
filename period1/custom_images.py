7c5dab40a7a2cfd022306a43fe19019e5d837cf6
Dan Schellenberg <schellenberg@gmail.com>
2017-11-20 09:06:35 -0600

    custom image demo

---
+++ period1/custom_images.py
@@ -0,0 +1,9 @@
+import microbit
+
+boat = microbit.Image("01010:" \
+                      "05050:" \
+                      "05050:" \
+                      "99999:" \
+                      "02220")
+
+microbit.display.show(boat)
